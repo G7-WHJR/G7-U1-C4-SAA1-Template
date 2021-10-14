557cd36fe350132a27c79dd7f1deb4110c7f23dc
unknown <raveen.anand@whitehatjr.com>
2021-10-14 19:27:12 +0530

    v2

--- Variable Conditionals/myscript.js
+++ Variable Conditionals/myscript.js
@@ -1,11 +1,12 @@
 function displayResult()
 {
     // 1. Take input from input box and store it in a variable named "moneySaved"
-     
+    var moneySaved= document.getElementById('').value;
 
-
+    //2. print "moneySaved" to the console.
+    console.log();
     
-    // 2. Check :
+    //SAA2 :  3. Check :
         //if moneySaved is greater than 100 then 
                 //print a message "Great Job! You saved alot of money!" 
         //else 
