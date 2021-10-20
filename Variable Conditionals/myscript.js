82679461b99b54e0b359ecb16464eb787afeac8e
unknown <raveen.anand@whitehatjr.com>
2021-10-20 18:33:56 +0530

    v2

--- Variable Conditionals/myscript.js
+++ Variable Conditionals/myscript.js
@@ -7,7 +7,7 @@ function displayResult()
     console.log();
     
     //SAA2 :  3. Check :
-        //if moneySaved is greater than 100 then 
+        //if moneySaved is greater than 50 then 
                 //print a message "Great Job! You saved alot of money!" 
         //else 
                 //print "You can do better! Try Saving more money next time!"
